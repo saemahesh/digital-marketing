94038395b802adc52c2cd9fed310eac15a5f24e9
unknown <saemahesh@gmail.com>
2025-03-15 21:05:48 +0530

    zoom link

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
-const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
 
 const nextConfiguration = {
   rewrites: async () => [
@@ -7,8 +7,19 @@ const nextConfiguration = {
       source: "/wa-sender",
       destination: "/wa-sender.html",
     },
+    {
+      source: "/join-meeting",
+      destination:
+        "https://us05web.zoom.us/j/82435259699?pwd=WZlDSowhUM4jcqybTO5a365mEE4aJB.1", // Replace with your Zoom meeting link
+      permanent: false, // Use false if the link may change
+    },
+    {
+      source: "/join-group",
+      destination: "https://chat.whatsapp.com/HG3RdNxRuqaLnVhdkPeScM", // Replace with your Zoom meeting link
+      permanent: false, // Use false if the link may change
+    },
   ],
-  target: 'serverless', //will output independent pages that don't require a monolithic server. It's only compatible with next start or Serverless deployment platforms (like ZEIT Now) — you cannot use the custom server API.
+  target: "serverless", //will output independent pages that don't require a monolithic server. It's only compatible with next start or Serverless deployment platforms (like ZEIT Now) — you cannot use the custom server API.
 };
 
 module.exports = withPlugins([optimizedImages], nextConfiguration);
