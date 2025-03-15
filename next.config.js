3357243725a04bfa7673ec17600b4f728590c941
unknown <saemahesh@gmail.com>
2025-03-15 21:15:58 +0530

    zoom link

--- next.config.js
+++ next.config.js
@@ -11,12 +11,10 @@ const nextConfiguration = {
       source: "/join-meeting",
       destination:
         "https://us05web.zoom.us/j/82435259699?pwd=WZlDSowhUM4jcqybTO5a365mEE4aJB.1",
-      statusCode: 307, // Use 307 for temporary redirects (instead of permanent false)
     },
     {
       source: "/join-group",
       destination: "https://chat.whatsapp.com/HG3RdNxRuqaLnVhdkPeScM",
-      statusCode: 307, // Temporary redirect
     },
   ],
   target: "serverless", //will output independent pages that don't require a monolithic server. It's only compatible with next start or Serverless deployment platforms (like ZEIT Now) — you cannot use the custom server API.
