cbf9961ce047aea8cc3d931896d438408f9b37da
unknown <saemahesh@gmail.com>
2025-03-15 21:40:38 +0530

    zoom link fix

--- next.config.js
+++ next.config.js
@@ -7,15 +7,6 @@ const nextConfiguration = {
       source: "/wa-sender",
       destination: "/wa-sender.html",
     },
-    {
-      source: "/join-meeting",
-      destination:
-        "https://us05web.zoom.us/j/82435259699?pwd=WZlDSowhUM4jcqybTO5a365mEE4aJB.1",
-    },
-    {
-      source: "/join-group",
-      destination: "https://chat.whatsapp.com/HG3RdNxRuqaLnVhdkPeScM",
-    },
   ],
   target: "serverless", //will output independent pages that don't require a monolithic server. It's only compatible with next start or Serverless deployment platforms (like ZEIT Now) — you cannot use the custom server API.
 };
